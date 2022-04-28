8f0bf4fbdc74daaefeaa0fe9b04d42ec416981cf
Nitin rathour <workinfoanalysis@gmail.com>
2022-04-28 21:39:41 +0530

    subscribe page active and connect my sql

--- layout/lib/subscribe.php
+++ layout/lib/subscribe.php
@@ -1,9 +1,46 @@
+<?php 
+include 'layout/database/db.php';
+
+$conn = mysqli_connect($servername, $username , $password, "subscriber");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+if (isset($_POST['subscribe'])) {
+    $email = $_POST['email'];
+    if (!$email) {
+        echo "please fill email";
+    } else {
+        $values = "'$email'";
+        $table = "email";
+        
+        $sql = insert('useremail', $table, $values);
+        $query = mysqli_query($conn, $sql);
+    }
+}
+?>
+
 <div class="auto-container">
-    <form action="#" class="subscribe-box flex-just-align">
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" class="subscribe-box" method="POST">
         <caption>
             <h1>Welcome to our <span>developer </span> family</h1>
         </caption>
-        <input type="email" name="email" id="email" placeholder="Enter your email..">
-        <button>Subscribe</button>
+        <input type="email" name="email" id="email" required placeholder="Enter your email..">
+        <input type="submit" name="subscribe" id="subscribe-btn" value="Subscribe">
+    
+        <?php
+if (isset($_POST['subscribe'])) {
+     if ($query) {
+        echo "Thanks for sbuscribe";
+    } else {
+        echo "Error addiing record". mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+?>
+        
+
     </form>
 </div>
+
